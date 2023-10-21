cdac6d99f353dacd2cc38219070682b8d058ade8
manpreetsingh04m <ms2404mk@gmail.com>
2023-10-21 22:03:39 +0530

    Super-War Part 2

--- src/app.js
+++ src/app.js
@@ -21,35 +21,54 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+for (let i = 0;i < players.length ; i++){
+    let hero_or_villain = i % 2 === 0 ? "hero" : "villain"
+    let obj={
+        name: players[i],
+        strength: getRandomStrength(),
+        image:`./images/super-${i+1}.png`,    
+        type: hero_or_villain,
+    }
+    detailedPlayers.push(obj);
+    // console.log("detailedPlayers: ", detailedPlayers);
 
+}
     return detailedPlayers;
 }
-
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    let randomValue = Math.ceil(Math.random() * 100);
+    return randomValue;
 }
-
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = "";
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].type === type) {
+            fragment += `<div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+            </div>`;
+        }
+      }
 
     return fragment;
 }
 // Display players in HTML
 const viewPlayers = (players) => {
 
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+    document.getElementById('heroes').innerHTML = buildPlayers(players, "hero");
+    document.getElementById('villains').innerHTML = buildPlayers(players, "villain");
 
 }
 
